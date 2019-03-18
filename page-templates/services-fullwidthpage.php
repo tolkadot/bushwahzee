e1d1263a96b65df4855f516de4b4c55ea30bd58c
tolkadot <tolkadot@gmail.com>
2019-03-18 20:33:57 +1100

    services seperator

--- page-templates/services-fullwidthpage.php
+++ page-templates/services-fullwidthpage.php
@@ -30,24 +30,30 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php if( have_rows('services') ):
 
  	// loop through the rows of data
+  $total_fields = count(get_field('services')); $count_fields = 1;
     while ( have_rows('services') ) : the_row(); ?>
-
-<div class="wrapper wrapper-md pt-0">
-  <div class="container">
-    <div class="row t-mb-50">
-      <div class="col-12 text-center">
-        <h2 class="d-flex justify-content-center section-header t-mb-30">
-        <?php the_sub_field('service_title'); ?>
-        </h2>
-      </div>
-      <div class="col-md-12">
-        <?php the_sub_field('service_description') ?>
-        <p class=" text-center"><a class='bush-button' href="/contact">Book Now</a></p>
-      </div>
-    </div>
-  </div>
-</div>
-
+        <div class="wrapper wrapper-md pt-0">
+          <div class="container">
+            <div class="row t-mb-50">
+              <div class="col-12 text-center">
+                <h2 class="d-flex justify-content-center section-header t-mb-30">
+                <?php the_sub_field('service_title'); ?>
+                </h2>
+              </div>
+              <div class="col-md-12">
+                <?php the_sub_field('service_description') ?>
+                <p class=" text-center"><a class='bush-button' href="/contact">Book Now</a></p>
+              </div>
+            </div>
+              <?php if($count_fields < $total_fields ){ ?>
+                <div class="col-12">
+                  <div class="sep-wrapper "><hr class="schools-sep">
+                  </div>
+                </div>
+              <?php } ?>
+          </div>
+        </div>
+      <?php  $count_fields++ ;?>
 <?php
     endwhile;
     endif;
